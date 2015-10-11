034f260fb8765c8e5a8c2e2bdbe9f2331d5b1db2
Mark Malone <mark.malone@orbitz.com>
2015-10-11 18:39:10 -0500

    update find dead executors to new version

--- findDeadExecutors.groovy
+++ findDeadExecutors.groovy
@@ -1,55 +1,21 @@
-def issue = false, dead = false, inactive=false, job_failure=false
-// get handle to out
+// get handle to build output
 def config = new HashMap()
 def bindings = getBinding()
 config.putAll(bindings.getVariables())
 def out = config['out']
 
-for (aSlave in hudson.model.Hudson.instance.slaves) {
-    if ( aSlave.getLabelString() == "release" ) {
-        issue = false
-        dead = false
-        inactive=false
-        lastBuild = aSlave.getComputer().getTimeline().getLastBuild();
-        println('==========================================================')
-        println("Name: ${aSlave.name}")
-        println("\tcomputer.isOffline: ${aSlave.getComputer().isOffline()}")
 
-        // check that a job had been run in the past hour
-        Calendar oneHourAgo = Calendar.getInstance()
-        oneHourAgo.add(Calendar.HOUR_OF_DAY, -1)
-        if (lastBuild?.getTimestamp() < oneHourAgo) {
-            println('\tLast build was: ' + lastBuild?.timestampString);
-            inactive=true
-        }
-        
-        if ( aSlave.getComputer().isOffline() ) {
-            def cause = aSlave.getComputer().getOfflineCause().toString()
-            println("\tcomputer.getOfflineCause  $cause" )
-            if ( cause && !cause.contains("null") && !cause.contains( "idle" ) && !cause.contains( "Disconnected" )) {
-                issue = true
-            }
-        } else {
-            // check if executor is dead
-            execList = aSlave.getComputer().getExecutors()
-            for( exec in execList ) {
-                if (exec.getCauseOfDeath()) {
-                    dead=true
-                    println("\tSlave ${aSlave.name} has a dead executor!!")
-                    exec.getCauseOfDeath().printStackTrace(out)
-                    println('\t================')
-                    println("\tYanking Executor!")
-                    exec.doYank()
-                }
-            }
-        }
-
-        def thr = Thread.currentThread()
-        def build = thr?.executable
-        if ( (issue&&inactive)||dead ) {
-            build.setResult( hudson.model.Result.FAILURE  )
-        } else {
-            build.setResult( hudson.model.Result.SUCCESS  )
-        }
+  jenkins.model.Jenkins.instance.getComputers().each { computer ->
+  // check if executor is dead
+  execList = computer.getExecutors()      
+  for( exec in execList ) {
+    if (exec.getCauseOfDeath()) {
+      println("\tNode: ${computer.getDisplayName()}, executer ${exec.getDisplayName()} is dead.")
+      println("Error:")
+      exec.getCauseOfDeath().printStackTrace(out) 
+      println('\n')
+      println("\tRemoving Dead Executor.")
+      exec.doYank()
     }
+  }
 }
